ccd9f5b4bf0ace782a301194e9fd068a6a62ffc0
Ghanashyam19 <gamerxgaurav44@gmail.com>
2026-03-20 15:24:43 +0900

    frontend deploy ready

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import API from "../api/api";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://notes-realtime-website.onrender.com");
 
 export default function Editor() {
   const { id } = useParams();
